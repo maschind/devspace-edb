792a46f7963d7b3d820998b4653c961238444d43
Michael Bang <mbang@redhat.com>
2025-06-06 13:48:35 +0200

    Added label part to samples

--- add-edb-to-samples-in-devspaces.sh
+++ add-edb-to-samples-in-devspaces.sh
@@ -1,3 +1,8 @@
 oc create configmap \
   getting-started-samples --from-file=sample-devfile.json \
   -n openshift-operators
+oc label configmap \
+  getting-started-samples \
+  app.kubernetes.io/part-of=che.eclipse.org \
+  app.kubernetes.io/component=getting-started-samples \
+  -n openshift-operators
